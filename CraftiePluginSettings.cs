ad6a4d41796e436865f13a341a39a9b9bea531a5
Roman <11330099+Dubzzzzy@users.noreply.github.com>
2020-10-16 11:15:07 +0300

    Added settings ctor

--- CraftiePluginSettings.cs
+++ CraftiePluginSettings.cs
@@ -1,4 +1,6 @@
-﻿using ExileCore.Shared.Attributes;
+﻿using System.Security.Cryptography;
+using System.Windows.Forms;
+using ExileCore.Shared.Attributes;
 using ExileCore.Shared.Interfaces;
 using ExileCore.Shared.Nodes;
 
@@ -6,6 +8,12 @@ namespace Craftie
 {
     public class CraftiePluginSettings : ISettings
     {
+        public CraftiePluginSettings()
+        {
+            Enable = new ToggleNode(false);
+            StartCraftingButton = new HotkeyNode(Keys.NumPad5);
+        }
+
         public ToggleNode Enable { get; set; }
 
         [Menu("Start Crafting")]
