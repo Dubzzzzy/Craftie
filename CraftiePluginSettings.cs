97c3686c9a2a7079f9c072254790e2f56f02351c
Roman <11330099+Dubzzzzy@users.noreply.github.com>
2020-10-16 10:44:11 +0300

    Initial commit

---
+++ CraftiePluginSettings.cs
@@ -0,0 +1,10 @@
+﻿using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace Craftie
+{
+    public class CraftiePluginSettings : ISettings
+    {
+        public ToggleNode Enable { get; set; }
+    }
+}
